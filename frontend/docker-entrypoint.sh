65db96d4ccaeb65f316ffd24be7dd28a837117bc
OWAIS NISAR <owaisnisar5748@gmail.com>
2026-07-02 20:04:25 +0530

    Sanitize frontend backend URL on startup

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+backend_url="${BACKEND_URL:-${PUBLIC_API_URL:-}}"
+backend_url="$(printf '%s' "$backend_url" | tr -d '\r' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//;s#/*$##')"
+
+if [ -n "$backend_url" ] && ! printf '%s' "$backend_url" | grep -Eq '^https?://'; then
+  backend_url="https://$backend_url"
+fi
+
+if [ -z "$backend_url" ]; then
+  backend_url="http://127.0.0.1:8000"
+fi
+
+export BACKEND_URL="$backend_url"
+
+printf 'window.__BAZARIO_CONFIG__ = { API_URL: "/api" };\n' > /usr/share/nginx/html/runtime-config.js
+envsubst '$PORT $BACKEND_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+exec nginx -g 'daemon off;'
